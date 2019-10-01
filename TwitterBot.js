de75bd93491f3f985b66308bd2f1f01943cc12fa
Filipe Marron <52511902+filipezz@users.noreply.github.com>
2019-10-01 17:33:54 -0300

    Update TwitterBot.js

--- TwitterBot.js
+++ TwitterBot.js
@@ -62,10 +62,10 @@ const dias = [
   const Twit = require('twit')
   var T = new Twit({
    
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
   timeout_ms: 60 * 1000 // optional HTTP request timeout to apply to all requests.
  })
   
